c1438d9190e67c3a71738ae14b7fffe64c01ce4d
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-18 19:33:06 -0300

    implementação cachorro

--- cachorro.py
+++ cachorro.py
@@ -1,3 +1,16 @@
+from mamifero import Mamifero
+from animal import Animal
 
+class Cachorro(Animal, Mamifero):
 
-class ...
+    def __init__(self, tamanho_passo: int, volume_som: int):
+        self().__init__(tamanho_passo, volume_som)
+        self.__deslocamento = 0
+
+    def latir(self):
+
+        return ("CACHORRO: PRODUZ SOM: ", self.__volume_som, " SOM: AU")
+
+    def mover(self):
+
+        return ("CACHORRO: DESLOCOU ", self.__deslocamento)

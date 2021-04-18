ccb5812f05b1f1df6e336a52caa4d16271b065ec
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-18 19:34:52 -0300

    definição tamanho passo e volume som cachorro

--- cachorro.py
+++ cachorro.py
@@ -6,6 +6,8 @@ class Cachorro(Animal, Mamifero):
     def __init__(self, tamanho_passo: int, volume_som: int):
         self().__init__(tamanho_passo, volume_som)
         self.__deslocamento = 0
+        self.__tamanho_passo = 3
+        self.__volume_som = 3
 
     def latir(self):
 

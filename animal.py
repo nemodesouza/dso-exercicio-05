23961941427ae4f4dce6f0d9bd37fde5b9af9574
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-18 18:27:57 -0300

    implementação classe abstrata animal

--- animal.py
+++ animal.py
@@ -1,11 +1,24 @@
+from abc import ABC, abstractmethod
+
 
 class Animal(ABC):
+    @abstractmethod
     def __init__(self, tamanho_passo: int):
-        ...
+        self.__tamanho_passo = tamanho_passo
 
+    @property
+    def tamanho_passo(self):
+        return self.__tamanho_passo
 
-    """Insira aqui os outros metodos"""
+    @tamanho_passo.setter
+    def tamanho_passo(self, tamanho_passo):
+        self.__tamanho_passo = tamanho_passo
+
+    @abstractmethod
+    def mover(self):
+        pass
 
     @abstractmethod
     def produzir_som(self):
         pass
+

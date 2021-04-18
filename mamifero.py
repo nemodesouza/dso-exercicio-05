a4f076853def40899ff7667316cf1413b7210e6b
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-18 18:55:04 -0300

    implementação do método mover na classe mamífero

--- mamifero.py
+++ mamifero.py
@@ -1,5 +1,30 @@
 from abc import ABC, abstractmethod
+from animal import Animal
 
-class Mamifero(...): 
-    
-    """Insira aqui todos atributos e metodos"""
+
+class Mamifero(ABC, Animal):
+
+    def __init__(self, tamanho_passo: int, volume_som: int):
+        # Mamifero(int volume_som, int tamanho_passo)
+        super().__init__(tamanho_passo)
+        self.__volume_som = volume_som
+        self.__deslocamento = 0
+
+    @property
+    def volume_som(self):
+        return self.__volume_som
+
+    @volume_som.setter
+    def volume_som(self, volume_som):
+        self.__volume_som = volume_som
+
+    @abstractmethod
+    def produzir_som(self):
+        pass
+
+    @abstractmethod
+    def mover(self, distancia):
+        if isinstance(distancia, int):
+            self.__deslocamento = self.__tamanho_passo * distancia
+
+        return self.__deslocamento

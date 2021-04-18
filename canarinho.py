f7f3bc14e1ceaad1775254ffced590fe606a855d
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-18 19:33:54 -0300

    implementação canarinho

--- canarinho.py
+++ canarinho.py
@@ -1,3 +1,16 @@
+from ave import Ave
+from animal import Animal
 
+class Canarinho(Animal, Ave):
 
-class ...
+    def __init__(self, tamanho_passo: int, altura_voo: int):
+        self().__init__(tamanho_passo, altura_voo)
+        self.__deslocamento = 0
+
+    def cantar(self):
+
+        return ("CANARINHO: PRODUZ SOM: PIU")
+
+    def mover(self):
+
+        return ("CANARINHO: DESLOCOU ", self.__deslocamento, "VOANDO")

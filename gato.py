9c95997eaec0ca0d7013aa513b563b6c340b1301
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-18 19:35:26 -0300

    definição tamanho passo e volume som gato

--- gato.py
+++ gato.py
@@ -6,6 +6,8 @@ class Gato(Animal, Mamifero):
     def __init__(self, tamanho_passo: int, volume_som: int):
         self().__init__(tamanho_passo, volume_som)
         self.__deslocamento = 0
+        self.__tamanho_passo = 2
+        self.__volume_som = 2
 
     def miar(self):
 

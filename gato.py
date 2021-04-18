a00b1785f15bf16dbbc0fb491f8ea169b6653d87
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-04-18 19:33:26 -0300

    implementação gato

--- gato.py
+++ gato.py
@@ -1,3 +1,16 @@
+from mamifero import Mamifero
+from animal import Animal
 
+class Gato(Animal, Mamifero):
 
-class ...
+    def __init__(self, tamanho_passo: int, volume_som: int):
+        self().__init__(tamanho_passo, volume_som)
+        self.__deslocamento = 0
+
+    def miar(self):
+
+        return ("CACHORRO: PRODUZ SOM: ", self.__volume_som, " SOM: MIAU")
+
+    def mover(self):
+
+        return ("CACHORRO: DESLOCOU ", self.__deslocamento)
